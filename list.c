3937b1bf148ec04f8705100f936a9f220c6cc288
Replit user <>
2023-08-22 23:47:15 +0000

    new try

--- list.c
+++ list.c
@@ -59,10 +59,24 @@ void * nextList(List * list) {
 }
 
 void * lastList(List * list) {
+  if (list->tail != NULL) {
+    list->current = list->tail;
+    return list->current->data;
+  }else {
+    fprintf(stderr, "Error: La lista está vacía.\n");
+    return NULL;
+    }
 
 }
 
 void * prevList(List * list) {
+  if (list->current != NULL && list->current->prev != NULL) {
+      list->current = list->current->prev;
+      return list->current->data;
+  }else {
+      fprintf(stderr, "Error: No hay nodo anterior o el nodo actual no está configurado.\n");
+      return NULL;
+    }
 
 }
 
