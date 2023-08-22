b596f1416bcd083125f91fef757cf5122fab639c
Replit user <>
2023-08-22 23:16:20 +0000

    new try

--- list.c
+++ list.c
@@ -45,7 +45,7 @@ void * firstList(List * list) {
     } else {
         fprintf(stderr, "Error: La lista está vacía.\n");
     }
-  return NULL;
+    return newList;
 }
 
 void * nextList(List * list) {
@@ -55,7 +55,7 @@ void * nextList(List * list) {
     } else {
       fprintf(stderr, "Error: No hay nodo siguiente o el nodo actual no está configurado.\n");
     }
-  return NULL;
+    return newList;
 }
 
 void * lastList(List * list) {
